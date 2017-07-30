69b111c9d29297952d2cfb3f4b24eb1b74aca6a0
nikrb <nik_rb@yahoo.com>
2017-07-30 23:29:31 +0100

    initial coding

--- server.js
+++ server.js
@@ -1,34 +1,26 @@
 const express = require('express');
-const moment = require( 'moment');
-
+const mongo = require('mongodb');
 const app = express();
 
-// console.log( `using port [${process.env.PORT}] env [${process.env.NODE_ENV}]`);
-app.set('port', (process.env.PORT || 8081));
-
-app.use('/', express.static(process.cwd() + '/public'));
-
-app.get( '/', (req,res) => {
-  res.sendFile( process.cwd() + '/public/index.html');
-});
-
-app.get( '/:p', (req,res) => {
-  let stamp = null;
-  let natural = null;
-  const ts = moment.unix( req.params.p);
-  if( ts.isValid()){
-    stamp = req.params.p;
-    natural = ts.format( "MMMM D, YYYY");
+mongo.connect( 'mongodb://localhost:27017/shorty', ( err, db) => {
+  if( err){
+    throw new Error( "mongo connect failed");
   } else {
-    const nd = moment( req.params.p, "MMMM D, YYYY");
-    if( nd.isValid()){
-      stamp = nd.unix();
-      natural = nd.format( "MMMM D, YYYY");
-    }
+    console.log( "connected to db");
   }
-  res.send( { unix: stamp, natural: natural});
-});
 
-app.listen(app.get('port'), () => {
-  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+  app.set('port', (process.env.PORT || 8080));
+  app.use('/', express.static(process.cwd() + '/public'));
+
+  app.get( '/', (req,res) => {
+    res.sendFile( process.cwd() + '/public/index.html');
+  });
+
+  app.get( '/new/:url', (req, res) => {
+    // do the magic
+  });
+
+  app.listen(app.get('port'), () => {
+    console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+  });
 });
