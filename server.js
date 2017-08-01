2b179a07744099f5b7e0fe57e6a07b03be080cb7
nikrb <nik_rb@yahoo.com>
2017-08-01 16:25:44 +0100

    validate new urls

--- server.js
+++ server.js
@@ -33,14 +33,20 @@ mongo.connect( 'mongodb://localhost:27017/shorty', ( err, db) => {
   });
 
   app.get( '/new/*', (req, res) => {
+    // validate the new url http://www.example.com
+    const re = /https{0,1}:\/\/www\.[a-zA-Z0-9]+\.com/;
     const ourl = req.params[0];
-    const col = db.collection( 'urls');
-    col.count().then( (count) => {
-      col.insert( {url:ourl, surl: count+1})
-      .then( (result) => {
-        res.send( {original_url: ourl, short_url: count+1});
+    if( re.test( ourl)){
+      const col = db.collection( 'urls');
+      col.count().then( (count) => {
+        col.insert( {url:ourl, surl: count+1})
+        .then( (result) => {
+          res.send( {original_url: ourl, short_url: count+1});
+        });
       });
-    });
+    } else {
+      res.send( { original_url: null, short_url: null});
+    }
   });
 
   app.listen(app.get('port'), () => {
